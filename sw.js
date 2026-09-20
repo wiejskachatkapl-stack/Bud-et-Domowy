3a467670e1c0d2f6d1aa1061a511485c364db1bc
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-09-20 16:27:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budzet-domowy-v1003';
+const CACHE = 'budzet-domowy-v1004';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest',
   './assets/icons/icon-192.png', './assets/icons/icon-512.png', './assets/images/home_v1003.png'
